0f236157f0e657e715d754c36d63582deaec7925
Sam Dawson <sda110@uclive.ac.nz>
2025-04-30 20:43:43 +1200

    quick styling for placeholder pages

--- website/src/utilities/Placeholder.tsx
+++ website/src/utilities/Placeholder.tsx
@@ -1,13 +1,17 @@
+import { StyledPlaceholderPage } from "./Placeholder.styles";
+
 interface Props {
 	placeholder: string;
 }
 
 export const Placeholder = ({ placeholder }: Props) => {
 	return (
-		<div>
-			<h1>{placeholder}</h1>
-			<p>This is the {placeholder} page.</p>
-			<p>It is currently under construction.</p>
-		</div>
+		<StyledPlaceholderPage>
+			<div>
+				<h1>{placeholder}</h1>
+				<p>This is the {placeholder} page.</p>
+				<p>It is currently under construction.</p>
+			</div>
+		</StyledPlaceholderPage>
 	);
 };
